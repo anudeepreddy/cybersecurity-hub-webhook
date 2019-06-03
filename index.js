5a3856440e5862c335d720471a66836920a19d5d
Anudeep Reddy <anudeepc85@gmail.com>
2019-06-03 02:48:11 +0000

    changed port to 80

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 const express = require('express')
+const request = require('request')
 const fs = require('fs')
 const ctf = require('./ctfs.js')
 const app = express()
-const port = 3000
+const port = 80
+var data=''
 app.use(express.json())
 /*ctf((data)=>{
-      console.log(data[0].title)  
+      console.log(data[0].title)
     })*/
 app.get('/',(req, res)=>{
     res.send("Hello world;)")
 })
 app.get('/get',(req,res)=>{
-    data=fs.readFileSync('./core/welcomeintent.json','utf-8')
-        res.send(JSON.parse(data))
+
 })
 app.post('/webhook',(req,res)=>{
     intent=req.body.queryResult.intent.displayName
     console.log(intent)
-    if(intent=="Default Welcome Intent"){  
+    if(intent=="Default Welcome Intent"){
         data=fs.readFileSync('./core/welcomeintent.json','utf-8')
         res.send(JSON.parse(data))
     }
     else if(intent=="ctf's"){
-        ctf((data)=>{
-            console.log(JSON.stringify(data))
-            res.send(data)
-        })
-        
+        let url='https://ctftime.org/api/v1/events/?limit=10'
     }
-
 })
 app.listen(port)
